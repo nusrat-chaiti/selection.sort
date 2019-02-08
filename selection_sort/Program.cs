44f64b25aaabd7d3a33f5d9058b10bfb7d4e2292
nusrat-chaiti <47456761+nusrat-chaiti@users.noreply.github.com>
2019-02-09 00:02:46 +0600

    Add files via upload

---
+++ selection_sort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selection_sort
+{
+    class sorting
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10] { 76, 16, 99, 67, 87, 43, 24, 12, 78, 34 };
+            int n = 10;
+            Console.WriteLine("selection sort:");
+            Console.Write("initial array is: \n");
+            for (int i = 0; i < n; i++)
+            { Console.Write(array[i] + "\n"); }
+
+            int temporary, smallest;
+            for (int i = 0; i < n - 1; i++)
+            {
+                smallest = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (array[j] < array[smallest])
+                    { smallest = j; }
+                }
+                temporary = array[smallest];
+                array[smallest] = array[i];
+                array[i] = temporary;
+
+
+            }
+
+            
+            Console.Write("sorted array is: \n");
+            for (int i = 0; i < n; i++)
+            { Console.Write(array[i] +"\n"); }
+            Console.ReadKey();
+        }
+    }
+}
